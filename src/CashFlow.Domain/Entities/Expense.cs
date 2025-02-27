c79272e903ec88e7559698c9e705222e1f4005cb
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2025-02-27 16:39:23 -0300

    Updating the project to .NET 9 and creating the Expense domain class

---
+++ src/CashFlow.Domain/Entities/Expense.cs
@@ -0,0 +1,12 @@
+﻿using CashFlow.Domain.Enums;
+
+namespace CashFlow.Domain.Entities;
+public class Expense
+{
+    public long Id { get; set; }
+    public string Tilte { get; set; } = string.Empty;
+    public string? Desciption {  get; set; } 
+    public DateTime Date { get; set; }
+    public decimal Amount { get; set; }
+    public PaymentType PaymentType {  get; set; }
+}
